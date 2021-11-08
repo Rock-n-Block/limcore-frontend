1b6a8f4296ff53e08eb3be4af5e6373e7c2a78c9
Gleb Chistyakov <gleb4ist@gmail.com>
2021-11-08 14:34:45 +0300

    imp i18n util

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import './utils/i18n';
+
 import './App.scss';
 
 export const App: React.FC = () => {
-  return (
-    <div className="">app</div>
-  );
+  return <div className="">app</div>;
 };
