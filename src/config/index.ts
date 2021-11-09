2657e878a886c8c730276425688a4d73a70ea19a
Gleb Chistyakov <gleb4ist@gmail.com>
2021-11-09 15:04:05 +0300

    change config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,67 +1,74 @@
-// import { chainsEnum, IConnectWallet, IContracts } from 'typings';
+import { IConnectWallet, IContracts } from 'typings';
 
-// import {
-//   erc20Abi,
-// } from './abi';
+import { erc20Abi } from './abi';
 
-// export const is_production = false;
+export const is_production = false;
 
-// export const chains: {
-//   [key: string]: {
-//     name: chainsEnum;
-//     chainId: number;
-//     provider: {
-//       [key: string]: any;
-//     };
-//     img?: any;
-//     explorer: string;
-//   };
-// } = {
-//   [chainsEnum.Tron]: {
-//     name: chainsEnum.Tron,
-//     chainId: is_production ? 1 : 0,
-//     provider: {
-//       TronLink: { name: 'TronLink' },
-//     },
-//     explorer: is_production ? 'https://tronscan.org' : 'https://shasta.tronscan.org',
-//   },
-// }
+export const chains: {
+  [key: string]: {
+    name: string;
+    chainId: number;
+    provider: {
+      [key: string]: any;
+    };
+    img?: any;
+  };
+} = {
+  Trust: {
+    name: 'Trust Wallet',
+    chainId: is_production ? 1 : 0,
+    provider: {
+      WalletConnect: {
+        name: 'WalletConnect',
+        useProvider: 'rpc',
+        provider: {
+          rpc: {
+            rpc: {
+              [is_production ? 1 : 4]: is_production
+                ? 'https://bsc-dataseed.binance.org/'
+                : 'https://data-seed-prebsc-2-s1.binance.org:8545/',
+            },
+            chainId: is_production ? 1 : 4,
+          },
+        },
+      },
+    },
+  },
+};
 
-// export const connectWallet = (
-//   chainName: chainsEnum,
-// ): IConnectWallet & {
-//   blockchains: Array<string>;
-// } => {
-//   const chain = chains[chainName];
+export const connectWallet = (
+  chainName: string,
+): IConnectWallet & {
+  blockchains: Array<string>;
+} => {
+  const chain = chains[chainName];
 
-//   return {
-//     wallets: ['MetaMask', 'WalletConnect'],
-//     blockchains: ['Ethereum', 'Binance Smart Chain', 'Polygon'],
-//     network: {
-//       name: chain.name.toString(),
-//       chainID: chain.chainId,
-//     },
-//     provider: chain.provider,
-//     settings: { providerType: true },
-//   };
-// };
+  return {
+    wallets: ['MetaMask', 'WalletConnect'],
+    blockchains: ['Ethereum', 'Binance Smart Chain', 'Polygon'],
+    network: {
+      name: chain.name.toString(),
+      chainID: chain.chainId,
+    },
+    provider: chain.provider,
+    settings: { providerType: true },
+  };
+};
 
-// export const contracts: IContracts = {
-//   type: is_production ? 'mainnet' : 'testnet',
-//   names: [],
-//   decimals: 18,
-//   params: {
-//     TOKEN: {
-//       mainnet: {
-//         address: '',
-//         abi: erc20Abi,
-//       },
-//       testnet: {
-//         address: '',
-//         abi: erc20Abi,
-//       },
-//     },
-//   },
-// };
-
-export default {};
+export const contracts: IContracts = {
+  type: is_production ? 'mainnet' : 'testnet',
+  names: [],
+  decimals: 18,
+  params: {
+    TOKEN: {
+      mainnet: {
+        address: '',
+        abi: erc20Abi,
+      },
+      testnet: {
+        address: '',
+        abi: erc20Abi,
+      },
+    },
+  },
+};
