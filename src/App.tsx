57ba2c84bd6b04bdefb1c7cb483fae90a072c997
Alexey Shevchenko <kamikozz@mail.ru>
2021-11-10 00:52:44 +0300

    fix: correct Button width to min-width: max-content; correct Header after layout's breaking changes; set body min-width: 320px

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Header } from 'containers';
 import { MainPage } from 'pages';
 
 import './styles/index.scss';
@@ -8,6 +9,8 @@ export const App: React.FC = () => {
   return (
     <div className="app">
       <div className="content">
+        <Header />
+
         <MainPage />
       </div>
     </div>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import cn from 'classnames';
 
-import { BuyWrapper, CurrentPrice, CurrentRound, Header, Preview } from 'containers';
+import { BuyWrapper, CurrentPrice, CurrentRound, Preview } from 'containers';
 
 import style from './main.module.scss';
 
 const Main: React.FC = () => {
   return (
-    <>
-      <Header />
-      <div className={style.main}>
-        <Preview />
-        <div className={cn(style.container, 'container')}>
-          <div className={cn(style.box_mini, style.box)}>
-            <CurrentRound days={5} allTokens={9191001} soldTokens={9191001 / 2} />
-            <CurrentPrice price={0.1} />
-          </div>
-          <div className={cn(style.box_big, style.box)}>
-            <BuyWrapper />
-          </div>
+    <div className={style.main}>
+      <Preview />
+      <div className={cn(style.container, 'container')}>
+        <div className={cn(style.box_mini, style.box)}>
+          <CurrentRound days={5} allTokens={9191001} soldTokens={9191001 / 2} />
+          <CurrentPrice price={0.1} />
+        </div>
+        <div className={cn(style.box_big, style.box)}>
+          <BuyWrapper />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
