150c5a9c79097b760f20eacade160d0dd53ba466
Gleb Chistyakov <gleb4ist@gmail.com>
2021-11-09 18:49:39 +0300

    lt-19

--- src/containers/CurrentRound/index.tsx
+++ src/containers/CurrentRound/index.tsx
@@ -15,7 +15,10 @@ const CurrentRound: React.FC<ICurrentRound> = () => {
       <h2 className={cn(style.title, 'text_bold')}>№1</h2>
       <h3 className={cn(style.subtitle, 'text_blue')}>Текущий раунд</h3>
       <div className={cn(style.text, 'text_sm')}>5 дней осталось</div>
-      <Progress percent={50} sectors={7} />
+      <div className={style.progress}>
+        <Progress percent={50} sectors={7} />
+      </div>
+      <div className={cn(style.tokens, 'text_md')}>1002 из 9191001 токенов продано </div>
     </div>
   );
 };
